9f692d830ae03799533f133b452a9985b61f4ff4
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-08-25 06:57:49 +0100

    Level1/array_rotation Done

--- Exam rank 04/Level1/array_rotation_detector.py
+++ Exam rank 04/Level1/array_rotation_detector.py
@@ -1,22 +1,3 @@
-# def array_rotation_detector(arr1: list[int], arr2: list[int]) -> bool:
-#     if arr1 is None and arr2 is None:
-#         return True
-#     elif arr1 == arr2:
-#         return True
-#     elif len(arr1) != len(arr2):
-#         return False
-
-#     n: int = len(arr1)
-
-#     # range from 1, because -0 is error and omits arr2
-#     for i in range(1, n):
-#         if (arr1[-i:] + arr1[:-i]) == arr2:
-#             return True
-#     return False
-
-
-
-
 """
 Write a function that determines if one array is a rotation
 of another array.
@@ -68,13 +49,11 @@ def array_rotation_detector(arr1: list[int], arr2: list[int]) -> bool:
         return False
     if (not arr1 and not arr2) or arr1 == arr2:
         return True
-    n = len(arr1)
-    new_arr = arr1 + arr1
-    for i in range(n):
-        if new_arr[i:i+n] == arr2:
+
+    for i in range(1, len(arr1)):
+        if arr1[-i:] + arr1[:-i] == arr2:
             return True
     return False
-
 
 
 print(f"True: {array_rotation_detector([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])}")
